c637d2cbba91b487346c5f4ebafbd48909cb07ef
Michael Klishin <mklishin@pivotal.io>
2017-08-23 19:19:34 -0400

    No foobars please

--- test/src/autocluster_tests.erl
+++ test/src/autocluster_tests.erl
@@ -65,13 +65,13 @@ initialize_backend_with_proxy_test_() ->
         meck:expect(application, ensure_all_started, fun (rabbitmq_aws) -> ok end),
         autocluster_testing:reset(),
         os:putenv("AUTOCLUSTER_TYPE", "aws"),
-        os:putenv("HTTP_PROXY", "foobar.com"),
-        os:putenv("HTTPS_PROXY", "foobar.com"),
+        os:putenv("HTTP_PROXY", "proxy.eng.megacorp.com"),
+        os:putenv("HTTPS_PROXY", "proxy.eng.megacorp.com"),
         os:putenv("PROXY_EXCLUSIONS", "0.0.0.0, 1.1.1.1"),
         inets:start(),
         {ok, _} = autocluster:initialize_backend(#startup_state{}),
-        {ok, {{"foobar.com", 80}, ["0.0.0.0", "1.1.1.1"]}} = httpc:get_option(proxy),
-        {ok, {{"foobar.com", 443}, ["0.0.0.0", "1.1.1.1"]}} = httpc:get_option(https_proxy),
+        {ok, {{"proxy.eng.megacorp.com", 80}, ["0.0.0.0", "1.1.1.1"]}} = httpc:get_option(proxy),
+        {ok, {{"proxy.eng.megacorp.com", 443}, ["0.0.0.0", "1.1.1.1"]}} = httpc:get_option(https_proxy),
         ?assert(meck:called(application, ensure_all_started, '_')),
         inets:stop()
     end).
