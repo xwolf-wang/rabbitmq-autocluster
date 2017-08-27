91a291d041069af86c801423027a551ac20c5163
Michael Klishin <mklishin@pivotal.io>
2017-08-27 15:34:32 -0600

    Ditto

--- src/autocluster_consul.erl
+++ src/autocluster_consul.erl
@@ -466,7 +466,7 @@ registration_body_maybe_add_check(Payload) ->
     -> list().
 registration_body_maybe_add_check(Payload, undefined) ->
     case registration_body_maybe_add_deregister([]) of
-        [{'Deregister_critical_service_after', _}]->
+        [{'DeregisterCriticalServiceAfter', _}]->
             autocluster_log:warning("Can't use Consul Deregister After without " ++
             "using TTL. The parameter CONSUL_DEREGISTER_AFTER will be ignored"),
             Payload;
@@ -496,7 +496,7 @@ registration_body_add_port(Payload) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Evaluate the configured value for the deregister_critical_service_after.
+%% Evaluate the configured value for the DeregisterCriticalServiceAfter.
 %% Consul removes the node after the timeout (If it is set)
 %% Check definition if it is set.
 %%
@@ -511,7 +511,7 @@ registration_body_maybe_add_deregister(Payload) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Evaluate the configured value for the deregister_critical_service_after.
+%% Evaluate the configured value for the DeregisterCriticalServiceAfter.
 %% Consul removes the node after the timeout (If it is set)
 %% Check definition if it is set.
 %% @end
